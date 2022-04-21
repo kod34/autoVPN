b8783085eeb6d5103167a9ce44ff096415456491
kod3a <5od3a.yes@protonmail.com>
2022-04-21 07:02:04 -0400

    Update

--- autovpn.py
+++ autovpn.py
@@ -21,6 +21,7 @@ banner = '''
   / _ \ | | | || __|/ _ \\\\ \ / / | |_) ||  \| |
  / ___ \| |_| || |_| (_) |\ V /  |  __/ | |\  |
 /_/   \_\\\\__,_| \__|\___/  \_/   |_|    |_| \_|
+				by kod34
                                                
 '''
 
@@ -36,14 +37,9 @@ c = 1
 dict_files ={}
 
 # Create directories
-try:
-	os.mkdir('pass_dumps/')
-except FileExistsError:
-	pass
-try:
-	os.mkdir('bundles/')
-except FileExistsError:
-	pass
+os.makedirs('pass_dumps/', exist_ok=True)
+os.makedirs('bundles/', exist_ok=True)
+
 
 # Get list elements from source code
 url_contents = urllib.request.urlopen(url+'/freevpn').read()
@@ -97,18 +93,27 @@ pass_img = urllib.request.urlopen(pass_url).read()
 with open('pass_dumps/pass.png', 'wb') as passfile:
 	passfile.write(pass_img)
 
-# Read image
-img = cv2.imread('pass_dumps/pass.png', cv2.IMREAD_GRAYSCALE)  
-thresh = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)[1]
-thresh = cv2.resize(thresh, (0,0), fx=2.2, fy=2.2)
 
-# Get password
-password = pytesseract.image_to_string(thresh, config = '--psm 6').strip()
+##### needs more work
+# # Read image
+# img = cv2.imread('pass_dumps/pass.png', cv2.IMREAD_GRAYSCALE)  
+# thresh = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)[1]
+# thresh = cv2.resize(thresh, (0,0), fx=2.2, fy=2.2)
+# # Get password
+# password = pytesseract.image_to_string(thresh, config = '--psm 6').strip()
 
+img = Image.open('pass_dumps/pass.png')
+img.show()
+
+try:
+	password = input("\nPassword: ")
+except ValueError:
+    
+	sys.exit(1)
 # Save credentials
 with open('creds.txt', 'w') as creds:
 	creds.write(username+"\n"+password)
 
 # Launch openvpn
 vpnfile = directory+file
-subprocess.run(['xterm', '-geometry', '110x24+0+0', '-hold', '-e', 'sudo', 'openvpn', '--config', vpnfile, '--auth-user-pass', 'creds.txt'])
+subprocess.Popen(['xterm', '-T', 'OpenVPN: '+str(os.path.splitext(file)[0]), '-geometry', '110x24+0+0', '-hold', '-e', 'sudo', 'openvpn', '--config', vpnfile, '--auth-user-pass', 'creds.txt'])
